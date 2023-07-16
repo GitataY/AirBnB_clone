c66dad0a3d9350cb5da92cee2452fdae839f7367
wamuyu <yvonnewamuyug@gmail.com>
2023-07-16 10:06:36 -0700

    models

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+state module
+Defines the state class
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """state class. A class to create a state"""
+    name = ""
+
